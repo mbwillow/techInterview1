60281449924c5d1d71ed508322f63811a95d0632
mbwillow <mbwillowgate@gmail.com>
2017-05-27 19:26:14 -0700

    initial file commit

---
+++ php/index.php
@@ -0,0 +1,22 @@
+<?php 
+
+/************************************************* 
+
+Purpose: This is an exercice to test your basic knowledge of interface and class in PHP oriented object.
+
+Instruction:
+
+1. Create an interface class called Car. It should contain only a single method, finalCost();
+
+2. Create another class, Chevy, that implements Car. It should have a protected variable called cost. It should have a constructor which take the cost in argument and set it. It should have the method finalCost() which take in argument a tax percentage and return the final cost of the car in $ using the cost set when the object is initialized
+
+3. Use the Chevy class to get the cost of a Chevy with a tax of 12% and the initial cost of 20000. Echo the cost in the screen.
+
+4. Please put all your code below, no need to create separate file. Do the runtime execution below as well. You can wrap it in HTML doc structure for bonus points.
+
+5. Bonus: if you have time, create a class called Ford which implements car and have the cost calculated. However this time, Ford has a handling fee attached to it of $1,000 to be calculated before tax.
+
+***************************************************/
+
+
+?>
